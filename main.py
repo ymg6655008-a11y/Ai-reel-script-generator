962fd1f602e45323738771beaf148814368c1f15
ymg6655008-a11y <ymg6655008@gmail.com>
2026-07-04 14:38:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ api_key = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=api_key)
 
 # मॉडल का नाम चेक करें (यही लिखें)
-model = genai.GenerativeModel('gemini-pro')
-
+model = genai.GenerativeModel('gemini-1.5-flash')
 st.title("AI Reel Script Generator")
 topic = st.text_input("ویڈیو کا ٹاپک لکھیں")
 
